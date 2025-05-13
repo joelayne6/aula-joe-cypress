92b918636779a32e106a179f22c5dd07f6bface4
joelayne maria <joelaynemaria@gmail.com>
2025-05-13 13:18:37 -0300

    primeiro comiit

---
+++ cypress/e2e/login.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+context('funcionalidade login', () =>{
+
+    it('Deve fazer login com sucesso' , () =>{
+
+       
+       
+        it('Deve exibir mensagem de erro ao inserir usuario ou senha invalidas', () =>{
+            
+        })
+    })
+})
